450dac23834ac0cbd4b9ed5d04a4a7b8d60742c6
Manroid38 <phamdoanmanu38@gmail.com>
2017-02-09 13:18:36 +0700

    Initial commit

---
+++ app/src/main/java/com/manroid/flashlight/MainActivity.java
@@ -0,0 +1,18 @@
+package com.manroid.flashlight;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView txtShow;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtShow = (TextView)findViewById(R.id.MarqueeText);
+        txtShow.setSelected(true);
+    }
+}
